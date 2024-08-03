392c216210823019c91942f544862b48865f1f2b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-03 23:42:10 +0100

    Add route for create devices

--- routes/devicesRoutes.js
+++ routes/devicesRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     getDevices,
     getSingleDevice,
+    createDevice,
 } from '../controllers/devicesController.js'
 
 export const router = express.Router();
@@ -9,6 +10,6 @@ export const router = express.Router();
 
 router.get('/', getDevices);
 router.get('/:id', getSingleDevice);
-// router.post('/', createDevice);
+router.post('/', createDevice);
 // router.put('/:id', updateDevice);
 // router.delete('/:id', deleteDevice);
