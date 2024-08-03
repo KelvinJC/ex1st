90f85f3a6295f4c3d4a50b6b36f375c555e7e676
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-03 23:42:39 +0100

    Add middleware to parse json requests

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { router as deviceRouter } from './routes/devicesRoutes.js' 
 
+
 const app = express();
 
-
+app.use(express.json())
 app.use('/api/devices', deviceRouter)
 
 
